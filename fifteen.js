b856a7ee9112695a806e0c8fb9cb0abee57167fa
gilisagreen <gilisagreen@gmail.com>
2013-10-27 09:46:21 -0500

    Update fifteen.js

--- fifteen.js
+++ fifteen.js
@@ -1,5 +1,5 @@
-var puzzle = new Array();
-var backPiece = new Array();
+var puzzle = [];
+var backPiece = [];
 
 
 window.onload = function() {
@@ -13,7 +13,7 @@ window.onload = function() {
         puzzle[i].style.float = "left";
        puzzle[i].style.backgroundSize = "400px 400px";
        
-       backPiece[i] = new Array();
+       backPiece[i] = [];
        backPiece[i][0] = right;
        backPiece[i][1] = top;
 
@@ -22,7 +22,7 @@ window.onload = function() {
        if (row === 4){top += 100; right = 0; row = 0; } else {right +=100;}
     }
 
-  freemove = document.createElement("div");
+  var freemove = document.createElement("div");
    $("puzzlearea").appendChild(freemove);
    freemove.addClassName("puzzlepiece");
     freemove.style.float = "left";
@@ -31,19 +31,7 @@ window.onload = function() {
 
    $("shufflebutton").observe('click', shufflePuzzle);
    movepiece();
-  complete();
-}
-
-var complete = function(){
-  var numArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-  var puzzle;
-  var c = numArray.toString();
-  var sp = puzzle.toString();
-
-  if (c == sp){
-    alert("You Won!");
-  }
-}
+};
 
 function shufflePuzzle(){
 		var numArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
@@ -65,7 +53,6 @@ function shufflePuzzle(){
       puzzlep[i].style.border = "2px solid black";
       puzzlep[i].style.backgroundImage = "url(background.jpg)";
       puzzlep[i].style.backgroundSize = "400px 400px";
-       //without the line below the puzzle shuffles fine. But I cant get it to work
       puzzlep[i].style.backgroundPosition = "-"+backPiece[test-1][0]+"px -"+backPiece[test-1][1]+"px";
           }
 			numArray[j] = x;
@@ -79,9 +66,6 @@ var movepiece = function(){
     var puzzlep = $$("#puzzlearea div");
     var yon = this.hasClassName('movablepiece');
     var blank = 0;
-    var bgcorrect =0;
-    //complete();
-    //alert(yon);
     if (yon){
       for (var i=0;i<puzzlep.length;i++){
         blank = puzzlep[i].innerHTML;
@@ -104,7 +88,7 @@ var movepiece = function(){
      }
   
    }
-         }
+         };
 
 
 var mopiece = function(){
@@ -122,25 +106,25 @@ var mopiece = function(){
   					case 0:
   						puzzlep[i+1].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 1:
   					case 2:
   						puzzlep[i-1].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 3:
   						puzzlep[i-1].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 4:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 5:
   					case 6:
@@ -150,43 +134,43 @@ var mopiece = function(){
   						puzzlep[i+4].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
   						puzzlep[i-1].addClassName("movablepiece");
-  						//alert("in mopiece loop");
+  						
               break;
   					case 7: 
   					case 11:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
   						puzzlep[i-1].addClassName("movablepiece");
-  						//alert("in mopiece loop");
+  						
               break;
   					case 8:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
   						puzzlep[i+4].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 12:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 13: 
   					case 14:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i-1].addClassName("movablepiece");
   						puzzlep[i+1].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					case 15:
   						puzzlep[i-4].addClassName("movablepiece");
   						puzzlep[i-1].addClassName("movablepiece");
-              //alert("in mopiece loop");
+              
   						break;
   					}
 
         	
   		}
 
-      Event.observe(puzzlep[i], 'click', movepiece); }  
+      puzzlep[i].observe('click', movepiece); }  
 
-  	}	
+  	}	;
